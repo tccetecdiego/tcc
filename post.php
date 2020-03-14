0527ace15da2aa360cd384e774a264c484621fbd
BraveRoyal <braveroyal2009@gmail.com>
2020-03-14 19:00:05 -0300

    melhorando pagina de compra

--- post.php
+++ post.php
@@ -8,15 +8,9 @@ $sql = new mysqli('localhost', 'root', '', 'tcc');
 $conteudo = json_decode(file_get_contents('php://input'));
 
 if($conteudo->funcionalidade == 'cadastrar'){
-    $dados = mysqli_query($sql,"SELECT * from item where nome = '$conteudo->nome'");
-    $vali = mysqli_num_rows($dados);
-    if($vali == 0){
-        $sql->query("INSERT INTO 
-                        item
-                        (nome, quantidade, minQuantidade, preco) VALUES('$conteudo->nome','$conteudo->quantidade','$conteudo->quantidadem','$conteudo->preco')");
-        $mensagem = 'Cadastrado com sucesso';
-    }else{
-        $mensagem = 'Produto ja cadastrado, insira um novo nome';
+    $convert = mysqli_query($sql,"CALL cadastrar('$conteudo->nome',$conteudo->quantidade,$conteudo->quantidadem,$conteudo->preco)");
+    while($coluna = mysqli_fetch_array($convert)) {
+        $mensagem = $coluna['mensagem'];
     }
 }
 else{
